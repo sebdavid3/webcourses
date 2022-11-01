7d7724a41ccf783257c252d33b28d13e7d5b660b
Sebastian <sdibanez@uninorte.edu.co>
2022-11-01 08:40:42 -0500

    ejercicios con numeros

--- Platzi/Escuela de DataScience/Fundamentos de Python/Numbers.py
+++ Platzi/Escuela de DataScience/Fundamentos de Python/Numbers.py
@@ -1,2 +1,36 @@
+import numbers
+
+
 lives = 3
 print(type(lives))
+
+lives = '3'
+print(type(lives))
+
+age = 12
+budget = 100
+
+temperature = 12.12
+print(type(temperature))
+
+lives = 2
+print(lives)
+lives = 1
+print(lives)
+
+lives = 12 + 15 
+print(lives)
+
+lives = lives - 1
+print(lives)
+
+lives -= 1
+print(lives)
+
+lives += 1
+
+numer = 5678765432346783212345666064.2
+print(numer)
+
+number_b = 0.0000000000000000001
+print(number_b)
